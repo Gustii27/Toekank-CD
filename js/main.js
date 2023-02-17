44384ffade7b012f677299b95d4b44314a4d6f71
Gustii27 <capuozzo.gustavo@gmail.com>
2023-02-17 15:03:28 -0300

    agregado opcion de compra con cuotas sin interes, y corrección de errores en el codigo

--- js/main.js
+++ js/main.js
@@ -26,15 +26,36 @@ const stockProductos = [
     },
 ]
 
+//--- Suma de productos agregados al carrito ---//
+
 let id1 = parseInt(prompt("Seleccione un ID del 1 al 5"));
-let id2 = parseInt(prompt("Seleccione otro ID distinto al primero, del 1 al 5"));
+let id2 = parseInt(prompt("Seleccione otro ID del 1 al 5"));
+
+// Se valida con un loop WHILE que los IDs ingresados esten en stockProductos
+
+while (!stockProductos.some(producto => producto.id === id1) || !stockProductos.some(producto => producto.id === id2)) {
+    
+    console.log("Uno o ambos IDs no son válidos");
+    
+    id1 = parseInt(prompt("Seleccione un ID del 1 al 5"));
+    id2 = parseInt(prompt("Seleccione otro ID del 1 al 5"));
+  }
 
 function sumarPreciosConsultados(stockProductos, id1, id2) {
+    
     let suma = 0;
+    let mismoId = 0;
+    
     stockProductos.forEach((producto) => {
-      if (producto.id === id1 || producto.id === id2) {
+        if (producto.id === id1 || producto.id === id2) {
+        suma += producto.precio;
+        mismoId++; //para que me guarde en una variable si los id ingresados son iguales
+      } 
+
+      if (mismoId === 1 && producto.id === id2) { //hace la comprobación de igualdad de 2 id, y los suma
         suma += producto.precio;
       }
+
     });
     return suma;
 }
@@ -42,3 +63,26 @@ function sumarPreciosConsultados(stockProductos, id1, id2) {
 let resultado = sumarPreciosConsultados(stockProductos, id1, id2);
 
 console.log(`El precio total de los productos con id ${id1} y ${id2} es de ${resultado} pesos`);
+
+
+
+//--- Posibilidad de pagar con tarjeta de crédito ---//
+
+let consultaCuotas = parseInt(prompt("Ingrese cantidad de cuotas; 1, 3, 6 y 12 cuotas sin interes"));
+let cantidadCuotas = [1, 3, 6, 12];
+
+// Se valida con un loop WHILE que la cantidad de cuotas que ingresa el usuario sea un valor disponible en el array, de no ser así el loop no se detendrá y solicitará ingresar un numero válido
+
+while (!cantidadCuotas.some(cuotas => cuotas === consultaCuotas)) {
+    console.log("La cantidad de cuotas ingresada no es válida");
+    consultaCuotas = parseInt(prompt("Ingrese cantidad de cuotas; 1, 3, 6 y 12 cuotas sin interes"));
+}
+
+function calculoCuotas(stockProductos, id1, id2, consultaCuotas){
+    let cuotas = sumarPreciosConsultados(stockProductos, id1, id2);
+    return (cuotas/consultaCuotas); 
+}
+
+let resultadoCuotas = calculoCuotas(stockProductos, id1, id2, consultaCuotas);
+
+console.log(`El precio total de los productos seleccionados es de ${resultado} en ${consultaCuotas} cuotas de ${resultadoCuotas} pesos`);
